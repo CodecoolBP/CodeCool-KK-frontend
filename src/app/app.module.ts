b6bb88e9a462d966e3372bbdd6afac77bf21d498
Akos Kolloros <kollorosa@gmail.com>
2019-03-12 14:38:21 +0100

    start of User

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import { UserItemComponent } from './components/user-item/user-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule
