ee580307ca8949f258bca23b9c69654b014c9c96
Akos Kolloros <kollorosa@gmail.com>
2019-03-27 10:52:22 +0100

    Login page style

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {User} from '../../../models/user/user';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+  }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+    // get return url from route parameters or default to '/'
+    this.returnUrl = '/';
+  }
+
+  get formControls() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+    const user: User = this.loginForm.value;
+    console.log(user.email);
   }
 
 }
