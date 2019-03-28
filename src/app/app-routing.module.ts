5fc25eb0e6fce31e4594b938b53c5190465c93a8
Akos Kolloros <kollorosa@gmail.com>
2019-03-28 13:57:51 +0100

    we won't get user page if not logged in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {AddUserComponent} from './components/user/add-user/add-user.component';
 import {LoginComponent} from './components/user/login/login.component';
+import {AuthGuard} from './guards/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent},
+  { path: '', component: HomeComponent, canActivate: [AuthGuard]},
   { path: 'register', component: AddUserComponent},
   { path: 'login', component: LoginComponent}
 ];
