4a558e07fd6b098f4fd6a4d03ec471279598e3aa
gaborkallos <gaborkallos@gmail.com>
2019-04-11 10:02:24 +0200

    merged, refactored

--- src/app/components/trip/trips/trips.component.ts
+++ src/app/components/trip/trips/trips.component.ts
@@ -8,6 +8,7 @@ import {Station} from '../../../models/station/station';
   templateUrl: './trips.component.html',
   styleUrls: ['./trips.component.css']
 })
+
 export class TripsComponent implements OnInit {
 
   trips: Trip[] = [];
@@ -17,9 +18,9 @@ export class TripsComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.tripService.getTrips().subscribe(trip => {
-        trip.forEach(t => {
-          this.trips.push(t);
+    this.tripService.getTrips().subscribe(trips => {
+        trips.forEach(trip => {
+          this.trips.push(trip);
         });
       }
     );
